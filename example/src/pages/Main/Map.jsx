828ed621da1dcb68642f61e4a62be070d033cb31
Oliver Roick <oliver@roick.email>
2022-05-05 13:51:25 +0100

    Example app: Add map

--- example/src/pages/Main/Map.jsx
+++ example/src/pages/Main/Map.jsx
@@ -1,8 +1,39 @@
+import { useRef, useState, useEffect } from 'react';
 import T from 'prop-types';
-import Panel from "../../layout/Panel";
+
+import mapbox from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+
+mapbox.accessToken = process.env.REACT_APP_MAPBOX_TOKEN || '';
 
 function Map ({ className }) {
-  return <Panel className={className}>Map</Panel>
+  const mapContainerRef = useRef();
+  const [ map, setMap ] = useState();
+
+  useEffect(() => {
+    const m = new mapbox.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [-99.31640625, 40.04443758460856],
+      zoom: 3,
+      dragRotate: false,
+      touchZoomRotate: false,
+      attributionControl: true,
+    });
+
+    const onLoad = () => setMap(m);
+    m.on('load', onLoad);
+
+    return () => {
+      m.off('load', onLoad);
+      if (map) {
+        map.remove();
+      }
+    };
+  }, []);
+
+  return <div ref={mapContainerRef} id='map' className={`h-screen ${className}`}>Map</div>
 }
 
 Map.propTypes = {
