4b5e39ce48132851e0c44ba4634a34732f4d9225
Oliver Roick <oliver@roick.email>
2023-11-22 14:59:38 +1100

    fix: Export useCollection and useItem

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import useStacSearch from './hooks/useStacSearch';
 import useCollections from './hooks/useCollections';
+import useCollection from './hooks/useCollection';
+import useItem from './hooks/useItem';
 import useStacApi from './hooks/useStacApi';
 import { StacApiProvider } from './context';
 
 export {
   useCollections,
+  useCollection,
+  useItem,
   useStacSearch,
   useStacApi,
   StacApiProvider,
